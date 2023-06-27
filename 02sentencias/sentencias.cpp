94eee9b70b7bdd6af27577d4eb089acc9b39693b
Noemi01v <a.villa.r031@edu.utc.mx>
2023-06-27 10:34:29 -0600

    02sentencias

---
+++ 02sentencias/sentencias.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+int i=1
+
+;int main(){
+*//do{;
+cout<<"Imprimir núeros"<<endl;
+int i=1;
+cout<<i<<endl;
+i--;
+while(i<11);
+
+
+//while mientras
+
+int count = 1;
+while(count<=10){
+	cout<<"Imprimir Números"<<endl;
+	
+	cout<<count<<endl;
+	count++;
+	}
+}
+*//
+
+int seleccion;
+
+do{
+	cout<<"\n Menu 1, 2, 3, y otra opción es salir"
+	cout<<"Opcion1\n"
+	cout<<"Opcion2\n"
+	cout<<"Opcion3\n"
+	cout<<"Salir\n"
+	
+	cin>>seleccion;
+	
+	switch(seleccion){
+		case 1:
+			cout<<"Esta es la opción 1";
+			break;
+		Case 2:
+		    cout<<"Esta es la opción 2";
+			break;
+		Case 3:
+		    cout<<"Esta es la opción 3";
+		default:
+		    cout<<"bye bye";
+			break;
+}
+}while((seleccion)>0)
